cac6fe07421ab30c20e590c21aa24d24c2f083e5
TProyectoWeb <TProyectoWeb@gmail.com>
2021-05-24 15:54:12 -0500

    Salida fecha.

--- JS/CtrlProc.js
+++ JS/CtrlProc.js
@@ -11,12 +11,13 @@ function procesa(){
   var boleta = parseInt(forma["boleta"].value),
   nombre = forma["nombre"].value,
   grupo = forma["grupo"].value,
-  materia = forma["materia"].value;
-  //var fecha = forma["fecha"].value;
-   
+  materia = forma["materia"].value,
+  fecha = new Date(txtFecha.value);
+  //fechaValueAsDate = txtFecha.valueAsDate;   
 
     salidaBoleta.value = "Tu boleta es: " + boleta;
     salidaNombre.value = "Tu nombre es: " + nombre;
     salidaGrupo.value = "Tu grupo es: " + grupo;
-    salidaMateria.value = "La materia es: " + materia;    
+    salidaMateria.value = "La materia es: " + materia;
+    salidaFecha.textContent = fecha.toLocaleString('en', { timeZone: 'UTC' });  
 }
