7d455d38cadb746c8e93154676a253677c583b62
TProyectoWeb <TProyectoWeb@gmail.com>
2021-05-24 16:07:30 -0500

    Actualización js y html.

--- JS/CtrlProc.js
+++ JS/CtrlProc.js
@@ -4,7 +4,8 @@ salidaBoleta = document.getElementById("salidaBoleta"),
 salidaNombre = document.getElementById("salidaNombre"),
 salidaGrupo = document.getElementById("salidaGrupo"),
 salidaMateria = document.getElementById("salidaMateria"),
-salidaFecha = document.getElementById("salidaFecha");
+salidaFecha = document.getElementById("salidaFecha"),
+txtFecha = forma["fecha"];
 
 
 function procesa(){
@@ -13,8 +14,6 @@ function procesa(){
   grupo = forma["grupo"].value,
   materia = forma["materia"].value,
   fecha = new Date(txtFecha.value);
-  //fechaValueAsDate = txtFecha.valueAsDate;   
-
     salidaBoleta.value = "Tu boleta es: " + boleta;
     salidaNombre.value = "Tu nombre es: " + nombre;
     salidaGrupo.value = "Tu grupo es: " + grupo;
